f6a3df1e3d507cac4eb492648496827fe424a935
kotaro_sakae <ksakae1216@gmail.com>
2021-06-13 15:43:06 +0900

    temp commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,52 @@
-import { TestBed, async } from '@angular/core/testing';
+// import { TestBed, async } from '@angular/core/testing';
+import { render, screen, fireEvent } from '@testing-library/angular';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
+// describe('AppComponent', () => {
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [
+//         AppComponent
+//       ],
+//     }).compileComponents();
+//   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
+//   it('should create the app', () => {
+//     const fixture = TestBed.createComponent(AppComponent);
+//     const app = fixture.debugElement.componentInstance;
+//     expect(app).toBeTruthy();
+//   });
 
-  it(`should have as title 'angular-testing-library'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-testing-library');
-  });
+//   it(`should have as title 'angular-testing-library'`, () => {
+//     const fixture = TestBed.createComponent(AppComponent);
+//     const app = fixture.debugElement.componentInstance;
+//     expect(app.title).toEqual('angular-testing-library');
+//   });
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-testing-library!');
-  });
-});
+//   it('should render title in a h1 tag', () => {
+//     const fixture = TestBed.createComponent(AppComponent);
+//     fixture.detectChanges();
+//     const compiled = fixture.debugElement.nativeElement;
+//     expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-testing-library!');
+//   });
+// });
+
+describe('Testing Library', () => {
+  test('should render counter', async () => {
+    await render(AppComponent, {
+      componentProperties: { counter: 5 },
+    })
+
+    expect(screen.getByText('Current Count: 5'));
+  })
+
+  test('should increment the counter on click', async () => {
+    await render(AppComponent, {
+      componentProperties: { counter: 5 },
+    })
+
+    fireEvent.click(screen.getByText('+'));
+
+    expect(screen.getByText('Current Count: 6'));
+  })
+})
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <button (click)="decrement()">-</button>
+    <span data-testid="count">Current Count: {{ counter }}</span>
+    <button (click)="increment()">+</button>
+  `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'angular-testing-library';
+
+  @Input() counter = 0
+
+  increment() {
+    this.counter += 1
+  }
+
+  decrement() {
+    this.counter -= 1
+  }
 }
